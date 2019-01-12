d17881940cda94e4a8a516335763e7deb1407d60
JC <johncunniff1248@gmail.com>
2019-01-12 12:01:57 -0500

    switch to new polybar

--- config/polybar/start.sh
+++ config/polybar/start.sh
@@ -4,13 +4,15 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar > /dev/null; do sleep 0.5; done
 
-polybar -c /root/.config/polybar/top example &
-polybar -c /root/.config/polybar/bottom bottom &
+# polybar -c /root/.config/polybar/top example &
+# polybar -c /root/.config/polybar/bottom bottom &
 
+polybar -c /root/.config/polybar/prmsrswt white &
 
 if [ -n "$(xrandr | grep DP2\ connected)" ]; then
-    polybar -c /root/.config/polybar/bottom.monitor bottom &
-    polybar -c /root/.config/polybar/top.monitor example &
+    polybar -c /root/.config/polybar/prmsrswt.monitor white &
+    # polybar -c /root/.config/polybar/bottom.monitor bottom &
+    # polybar -c /root/.config/polybar/top.monitor example &
 fi
 
 
