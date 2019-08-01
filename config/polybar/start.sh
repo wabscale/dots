40704cb4630ddc3f0de91eff87dd18c4fc7876fa
big_J <johncunniff1248@gmail.com>
2019-08-01 12:54:39 +0000

    Ra stuff

--- config/polybar/start.sh
+++ config/polybar/start.sh
@@ -8,10 +8,7 @@ POLYBAR_HOME=~/.config/polybar
 
 for i in `xrandr | nice grep ' connected' | awk '{print $1}'`; do
     echo $i
-    if [ -f "${POLYBAR_HOME}/prmsrswt.${i}" ]; then
-        echo "1"
-        polybar -c ${POLYBAR_HOME}/prmsrswt.$i white &
-    fi
+    DISP="${i}" polybar -c ${POLYBAR_HOME}/prmsrswt.conf white &
 done
 
 
