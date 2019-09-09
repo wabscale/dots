2c2e338d8dda028b21b2a727237337a9677f8f6e
big_J <johncunniff1248@gmail.com>
2019-09-09 15:21:09 +0000

    add quick setup file

---
+++ qs.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+if which apt-get; then
+    sudo apt-get update
+    sudo apt-get install -y git zsh python3 python3-pip
+elif which yum; then
+    sudo yum update
+    sudo yum install git zsh python3
+fi
+
+if [ -d ~/.oh-my-zsh ]; then rm -rf ~/.oh-my-zsh; fi
+if [ -d ~/dots ]; then rm -rf ~/dots; fi
+if [ -f ~/.zshrc ]; then rm ~/.zshrc; fi
+
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://gitlab.com/b1g_J/dots.git ~/dots
+
+ln -s ~/dots/zsh/.zshrc ~/.zshrc
+
+
+echo "export TERM=xterm-256color" >> ~/dots/zsh/.env_vars
+echo "export ZSH_THEME=gnzh" >> ~/dots/zsh/.env_vars
+echo "export SKIP_BANNER=true" >> ~/dots/zsh/.env_vars
+
